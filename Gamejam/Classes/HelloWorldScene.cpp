aad92a5c8b4c978b2f3f8fa8fd0077c97035cc0e
takumi maeoka <takumi.maeoka4@gmail.com>
2017-04-10 11:25:18 +0900

    スワイプ処理のサンプル

--- Gamejam/Classes/HelloWorldScene.cpp
+++ Gamejam/Classes/HelloWorldScene.cpp
@@ -6,6 +6,8 @@ USING_NS_CC;
 
 using namespace cocostudio::timeline;
 
+
+
 Scene* HelloWorld::createScene()
 {
     // 'scene' is an autorelease object
@@ -83,10 +85,176 @@ bool HelloWorld::init()
     {
         return false;
     }
+
+	EventListenerTouchOneByOne* listener = EventListenerTouchOneByOne::create();
+
+	listener->onTouchBegan = CC_CALLBACK_2(HelloWorld::onTouchBegan, this);
+	listener->onTouchMoved = CC_CALLBACK_2(HelloWorld::onTouchMoved, this);
+	listener->onTouchEnded = CC_CALLBACK_2(HelloWorld::onTouchEnded, this);
+	this->getEventDispatcher()->addEventListenerWithSceneGraphPriority(listener, this);
+
     
     auto rootNode = CSLoader::createNode("MainScene.csb");
-
     addChild(rootNode);
 
+	ninjaNode = Node::create();
+	this->addChild(ninjaNode);
+
+	ninja = Sprite::create("ninja.png");
+	ninja->setPosition(Vec2(480.0f, 320.0f));
+	ninja->setFlipX(true);
+	ninja->setScale(0.4);
+	ninjaNode->addChild(ninja);
+
+	mato = Sprite::create("mato.png");
+	mato->setPosition(Vec2(880.0f, 320.0f));
+	this->addChild(mato);
+
+
+
+	//syuriken = Sprite::create("syuriken.png");
+	//syuriken->setPosition(Vec2(480.0f, 320.0f));
+	//syuriken->setScale(0.1f);
+
+	//ninjaNode->addChild(syuriken);
+
+	scheduleUpdate();
+
+
     return true;
+}
+
+
+
+bool HelloWorld::onTouchBegan(cocos2d::Touch * touch, cocos2d::Event * unused_event)
+{
+
+	Vec2 touch_pos = touch->getLocation();
+
+	ninja->setPosition(touch_pos);
+
+
+	syuriken = Sprite::create("syuriken.png");
+	syuriken->setPosition(touch_pos);
+	syuriken->setScale(0.1f);
+	syuriken->setTag(10);
+	ninjaNode->addChild(syuriken);
+
+
+	MoveBy* action1 = MoveBy::create(0.5f,Vec2(1000.0f,0.0f));
+	RotateBy* action2 = RotateBy::create(3.0f, 360.0f * 100);
+	
+	Spawn* action3 = Spawn::create(action1, action2, nullptr);
+
+
+	syuriken->runAction(action3);
+	
+
+	return true;
+}
+
+void HelloWorld::onTouchMoved(cocos2d::Touch * touch, cocos2d::Event * unused_event)
+{
+	Vec2 touch_pos = touch->getLocation();
+
+	ninja->setPosition(touch_pos);
+
+
+	syuriken = Sprite::create("syuriken.png");
+	syuriken->setPosition(Vec2(touch_pos.x + 50.0f,touch_pos.y -10.0f));
+	syuriken->setScale(0.1f);
+
+	syuriken->setTag(10);
+
+	ninjaNode->addChild(syuriken);
+
+	MoveBy* action1 = MoveBy::create(0.5f, Vec2(1000.0f, 0.0f));
+	RotateBy* action2 = RotateBy::create(3.0f, 360.0f * 20);
+
+	Spawn* action3 = Spawn::create(action1, action2, nullptr);
+
+
+	syuriken->runAction(action3);
+
+
+	syuriken->runAction(action1);
+
+	prevpos = touch->getPreviousLocation();
+	currentpos = touch->getLocation(); //get previous positon
+
+	return;
+}
+
+// タップが終了したときの処理
+void HelloWorld::onTouchEnded(Touch* touch, Event* unused_event)
+{
+	double distance = ccpDistance(currentpos, prevpos);
+	if (distance>1) { //スワイプ最新二点間距離３以上
+		log("swiped?n"); //check
+		velocity = ccpSub(currentpos, prevpos); //currentpos-prevpos
+		Size bgSize = Director::sharedDirector()->getWinSize(); //画面サイズ
+		MoveBy* throwanimation = MoveBy::create(0.1, velocity); //0.1秒にvelocityだけ進む
+		ico = Sprite::create("ninja.png"); //画像読み込み
+		ico->setPosition(currentpos); //画像位置設定
+		this->addChild(ico); //画面張り付け thisは自分自身のポインタ
+		int countx = (difference(bgSize.width, currentpos.x)) / (velocity.x);
+		int county = (difference(bgSize.height, currentpos.y)) / (velocity.y);
+		Repeat *repeat = Repeat::create(throwanimation, countx<county ? countx : county); //適切な回数でリピート
+		ico->runAction(repeat); //アニメーションスタート
+		repeat->setTag(0); //animetionタグセット
+	}
+	return;
+}
+
+int HelloWorld::difference(int x, int y) { //差分計算
+	return x>y ? x - y : y - x;
+}
+
+void HelloWorld::update(float delta)
+{
+	if (syuriken != nullptr)
+	{
+		//if (syuriken->getPosition().x  >= 950.0f)
+		//{
+		//	syuriken->stopActionByTag(1);
+		//	syuriken->setPosition(Vec2(950.0f,syuriken->getPosition().y));
+		//}
+
+
+
+		Sprite* spr;
+
+
+		for (auto child:ninjaNode->getChildren())
+		{
+			spr = (Sprite*)child;
+			if (spr->getTag() == 10)
+			{
+				
+				Rect rect_syuriken = spr->getBoundingBox();
+				Rect rect_mato = mato->boundingBox();
+				Node* parent;
+				parent = spr->getParent();
+				rect_syuriken = RectApplyAffineTransform(rect_syuriken, parent->getNodeToWorldAffineTransform());
+				parent = mato->getParent();
+				rect_mato = RectApplyAffineTransform(rect_mato, parent->getNodeToWorldAffineTransform());
+
+				bool hit = rect_mato.intersectsRect(rect_syuriken);
+
+				if (hit)
+				{
+					spr->stopAllActions();
+				}
+
+
+				if (spr->getPosition().x >= 950.0f)
+				{
+					spr->stopAllActions();
+					spr->setPosition(Vec2(950.0f, spr->getPosition().y));
+				}
+
+			}
+
+		}
+	}
 }
--- Gamejam/Classes/HelloWorldScene.h
+++ Gamejam/Classes/HelloWorldScene.h
@@ -12,8 +12,27 @@ public:
     // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
     virtual bool init();
 
+	bool onTouchBegan(cocos2d::Touch* touch, cocos2d::Event* unused_event);
+	void onTouchMoved(cocos2d::Touch* touch, cocos2d::Event* unused_event);
+	void onTouchEnded(cocos2d::Touch* touch, cocos2d::Event* unused_event);
+
+	void update(float delta) override;
+
     // implement the "static create()" method manually
     CREATE_FUNC(HelloWorld);
+
+private:
+
+	cocos2d::Node* ninjaNode;
+	cocos2d::Sprite* ninja;
+	cocos2d::Sprite* syuriken;
+
+	cocos2d::Sprite* mato;
+
+	int difference(int, int);
+	cocos2d::Point currentpos, prevpos, velocity; //座標保存クラスと速度ベクトル管理クラス
+	cocos2d::Sprite* ico; //画像制御
+
 };
 
 #endif // __HELLOWORLD_SCENE_H__
