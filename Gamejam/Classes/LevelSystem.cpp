5b3816773840b6dfb1dfd335cb0c22eb42fed456
asakawatasuku <akuma.fairy.tail@gmail.com>
2017-04-10 15:45:12 +0900

    レベルの画像の追加　コードの変更

--- Gamejam/Classes/LevelSystem.cpp
+++ Gamejam/Classes/LevelSystem.cpp
@@ -31,9 +31,20 @@ bool LevelSystem::init()
 		return false;
 	}
 
+	// レベルの情報の初期化
 	m_level_info[4] = { 0 };
 
+	// レベルの初期化
 	m_level = 0;
+
+	// Lvの画像の初期化
+	m_lvnum = Sprite::create("Lv.png");
+	m_lvnum->setPosition(Vec2(115.0f, 150.0f));
+	this->addChild(m_lvnum);
+
+	// 数字の画像の初期化
+	m_lv = Sprite::create("one.png");
+	this->addChild(m_lv);
 
 	return true;
 }
@@ -48,18 +59,17 @@ void LevelSystem::set_level(int lv, int count, float beatspd)
 }
 
 
-// レベルを変動する関数
+// レベルを上げる関数
 void LevelSystem::up_level()
 {
 	m_level++;
 }
 
-// レベルを変動する関数
+// レベルを下げる関数
 void LevelSystem::down_level()
 {
 	m_level--;
 }
-
 
 
 // 現在のレベルのを取得する関数
@@ -72,6 +82,51 @@ int LevelSystem::get_level() const
 // 現在のレベルの情報を取得する関数
 LEVEL LevelSystem::get_level_info() const
 {
-	return m_level_info[m_level];
+	return m_level_info[m_level - 1];
+}
+
+
+// レベルを表示する関数
+void LevelSystem::show_level()
+{
+	// 判定
+	if (m_lv != nullptr)
+	{
+		// 削除
+		m_lv->removeFromParent();
+		m_lv = nullptr;
+
+		// レベルによって表示する画像を変更
+		switch (m_level)
+		{
+			case 1:
+				m_lv = Sprite::create("one.png");
+				this->addChild(m_lv);
+				break;
+			case 2:
+				m_lv = Sprite::create("two.png");
+				this->addChild(m_lv);
+				break;
+			case 3:
+				m_lv = Sprite::create("three.png");
+				this->addChild(m_lv);
+				break;
+			case 4:
+				m_lv = Sprite::create("four.png");
+				this->addChild(m_lv);
+				break;
+			case 5:
+				m_lv = Sprite::create("five.png");
+				this->addChild(m_lv);
+				break;
+		}
+		set_pos(150.0f,150.0f);
+	}
+}
+
+// 画像の場所をセットする関数
+void LevelSystem::set_pos(float x, float y)
+{
+	m_lv->setPosition(Vec2(x,y));
 }
 
--- Gamejam/Classes/LevelSystem.h
+++ Gamejam/Classes/LevelSystem.h
@@ -8,7 +8,7 @@
 // 構造体宣言
 struct LEVEL 
 {
-	int count;			// カウント
+	int count;				// カウント
 	float press_time;		// 時間
 };
 
@@ -18,17 +18,24 @@ class LevelSystem : public cocos2d::Node
 	private:
 		LEVEL m_level_info[5];		// レベルの情報
 		int m_level;				// レベル
+		cocos2d::Sprite* m_lv;		// Lvのスプライト
+		cocos2d::Sprite* m_lvnum;	// 数字のスプライト
 
 	public:
-		static LevelSystem * create();		// creat関数
-		bool LevelSystem::init();			// init関数
+		static LevelSystem* create();		// creat関数
+		bool init();						// init関数
 		
 		void set_level(int lv, int count, float beatspd);	// レベルの情報をセットする関数
 		void up_level();									// レベルを上げる関数
 		void down_level();									// レベルを下げる関数
 
-		int get_level() const;									//	現在のレベルを取得
+		int get_level() const;								// 現在のレベルを取得
 
-		LEVEL get_level_info() const;							// 現在のレベルの情報を取得する関数
+		LEVEL get_level_info() const;						// 現在のレベルの情報を取得する関数
+
+		void show_level();									// レベルを表示する関数
+
+		void set_pos(float x,float y);						// 画像の場所をセットする関数
+
 };
 #endif
