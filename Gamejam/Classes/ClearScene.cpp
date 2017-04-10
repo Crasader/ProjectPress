2728f2c1f27b262fa1387757b0b2bd0f5018aef0
asakawatasuku <akuma.fairy.tail@gmail.com>
2017-04-11 01:33:12 +0900

    クリアシーンの画像とコードの変更

--- Gamejam/Classes/ClearScene.cpp
+++ Gamejam/Classes/ClearScene.cpp
@@ -15,12 +15,17 @@ bool ClearScene::init()
 		return false;
 	}
 
+	/*m_clearbg = nullptr;*/
+	m_clearbg = Sprite::create("big.png");
+	set_pos(640 / 2, 960 / 2);
+	this->addChild(m_clearbg);
+
 	// 毎フレーム更新を有効化
 	scheduleUpdate();
 
 	//ボタンを作成する
 	ui::Button* button = ui::Button::create("Titlebutton.png");
-	button->setPosition(Vec2(660, 420));
+	button->setPosition(Vec2(640/2, 960/2-350));
 	this->addChild(button);
 
 	//ui::Button* button = ui::Button::create("Playbutton.png");
@@ -65,3 +70,34 @@ bool ClearScene::onButtonClick(Ref* ref)
 //
 //	return true;
 //}
+
+// クリア時の数によって画像が変更される関数
+void ClearScene::change_clear(int clear_cnt)
+{
+	// 削除
+	m_clearbg->removeFromParent();
+	
+	// 判定
+	if (clear_cnt < 50)
+	{
+		m_clearbg = Sprite::create("small.png");
+		this->addChild(m_clearbg);
+	}
+	else if (clear_cnt >= 50)
+	{
+		m_clearbg = Sprite::create("normal.png");
+		this->addChild(m_clearbg);
+	}
+	else if (clear_cnt > 100)
+	{
+		m_clearbg = Sprite::create("big.png");
+		this->addChild(m_clearbg);
+	}
+	set_pos(640/2,960/2);
+}
+
+// 描画位置を設定する関数
+void ClearScene::set_pos(float x,float y)
+{
+	m_clearbg->setPosition(Vec2(x, y));
+}
--- Gamejam/Classes/ClearScene.h
+++ Gamejam/Classes/ClearScene.h
@@ -15,4 +15,11 @@ public:
 
 	bool onButtonClick(Ref* ref);
 	//bool onPlayButtonClick(Ref* ref);
+
+	void change_clear(int clear_cnt);	// クリア時の数によって画像が変更される関数
+	void set_pos(float x, float y);		// 描画位置を設定する関数
+
+
+	cocos2d::Sprite* m_clearbg;			// クリア画像のスプライト
+
 };
